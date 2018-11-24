b14c27ad14981dce230dc6cf8f09e16a45d8426b
Alexander Popov <rastopyakin@gmail.com>
2018-11-25 04:01:25 +0700

    Add basic worker class

--- examples/ex1.cpp
+++ examples/ex1.cpp
@@ -1,7 +1,47 @@
+// simple usage example of two workers
+// calculating integral of e^x - 1 integrand.
 #include <iostream>
+#include <cmath>
+#include <chrono>
+
 #include "mcint.hpp"
+
+namespace chr=std::chrono;
 
 int main(int argc, char *argv[]) {
 
+    auto f = [] (auto x) {return exp(x)+1;};
+    std::size_t calls_per_update_num = 1e6;
+    mc::worker<decltype(f), double>
+        wk{f, calls_per_update_num}, wk1{f, calls_per_update_num};
+
+    auto start = std::chrono::steady_clock::now();
+
+    auto finish = start + std::chrono::seconds(10);
+
+    auto t = finish - std::chrono::steady_clock::now();
+    std::cout.precision(10);
+    while (t.count() > 0) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        auto s1 = wk.get_current_stat();
+        auto s2 = wk1.get_current_stat();
+
+        std::size_t total_calls  = s1.calls_num + s2.calls_num;
+        double estimate = (s1.M1*s1.calls_num + s2.M1*s2.calls_num)/total_calls;
+        double variance_estimate = ((s1.M2*s1.calls_num + s2.M2*s2.calls_num)/total_calls -
+                                    estimate*estimate)*total_calls/(total_calls - 1);
+        double error = std::abs(estimate - std::exp(1));
+        double error_estimate = std::sqrt(variance_estimate/total_calls);
+        std::cout << "current estimate: "  << estimate  << std::endl;
+        std::cout << "variance estimate: " << variance_estimate << std::endl;
+        std::cout << "error: " << error << std::endl;
+        std::cout << "error estimate: " << error_estimate << std::endl;
+        std::cout << "ratio: " << error/error_estimate << std::endl;
+        std::cout << "calls number: " << total_calls << std::endl;
+        std::cout << std::endl;
+
+        t = finish - std::chrono::steady_clock::now();
+    }
+    wk.stop(); wk1.stop();
     return 0;
 }
--- include/mcint.hpp
+++ include/mcint.hpp
@@ -1,6 +1,84 @@
 #ifndef MCINT_HPP
 #define MCINT_HPP
 
+#include <thread>
+#include <random>
+#include <atomic>
+#include <mutex>
+#include <iostream>
 
+namespace mc {
+
+    template <class ValType>
+    struct mc_chunk {
+        ValType M1 {};         // mean value
+        ValType M2 {};         // squared mean value
+        std::size_t  calls_num = 0;
+    };
+
+    template <class Func, class ValType, class ArgType = ValType,
+              class Gen = std::mt19937_64>
+    class worker {
+    public:
+        worker(const Func& f, std::size_t calls_num) :
+            integrand{f}, calls_per_chunk(calls_num) {
+                seed = seed_counter++;
+                work_th = std::thread{&worker::run, this, seed_counter};
+            }
+        ~worker() {
+            work_th.join();
+        }
+        mc_chunk<ValType> get_current_stat() const {
+            std::lock_guard<std::mutex> lk{lock};
+            return current;
+        }
+        void stop() {
+            stop_flag.store(true);
+        }
+    private:
+        void run(typename Gen::result_type seed) {
+            Gen gen(seed);
+            std::size_t chunks_num = 0;
+            std::uniform_real_distribution<ArgType> ur{0.0, 1.0};
+            while (!stop_flag.load()) {
+                ValType M1 = 0;
+                ValType M2 = 0;
+                for (int i = 0; i < calls_per_chunk; i++) {
+                    ValType val = integrand(ur(gen));
+                    M1 += val;
+                    M2 += val*val;
+                }
+                chunks_num++;
+                M1 /= calls_per_chunk;
+                M2 /= calls_per_chunk;
+                update(M1, M2, chunks_num - 1); // pass num of current chunks
+            }
+        }
+
+        void update(ValType M1, ValType M2, std::size_t chunks_num) {
+            std::lock_guard<std::mutex> lk{lock};
+            current.M1 = (chunks_num*current.M1 + M1)/(chunks_num + 1);
+            current.M2 = (chunks_num*current.M2 + M2)/(chunks_num + 1);
+            current.calls_num += calls_per_chunk;
+        }
+    private:
+        static typename Gen::result_type seed_counter;
+        typename Gen::result_type seed;
+        mutable std::mutex lock;
+        mc_chunk<ValType> current;
+        Func integrand;
+        std::size_t calls_per_chunk;
+        std::thread work_th;
+        std::atomic<bool> stop_flag {false};
+    };
+
+    class collector {};
+
+    class naive_integrator {};
+
+    template <class Func, class ValType, class ArgType, class Gen>
+    typename Gen::result_type worker<Func, ValType, ArgType, Gen>::seed_counter = 0;
+
+}
 
 #endif /* MCINT_HPP */
